ba4443cf7ed63c46a50223cf18d19a6d3349ec08
Danielv123 <tranen.bananen@gmail.com>
2015-03-11 16:47:21 +0100

    Tried to make it pause after executing, not working

--- main.cpp
+++ main.cpp
@@ -12,9 +12,12 @@ int main()
 
     cout<<"x+y = z\nPlease input x.\n";
     cin>>number1;
+    cin.clear();
     cout<<number1<<"+y = z\nPlease input y.\n";
     cin>>number2;
+    cin.clear();
     number3 = number1 + number2;
     cout<<number1<<"+"<<number2<<" = "<<number3<<"\nMATH COMPLETED\n";
+    std::cin.ignore();
     return 0;
 }
