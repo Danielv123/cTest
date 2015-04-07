c64c891c0d6e40405c24fe383aa0b7d7044fcf8a
Danielv123 <tranen.bananen@gmail.com>
2015-04-07 23:38:01 +0200

    Comment added

--- main.cpp
+++ main.cpp
@@ -11,10 +11,12 @@ int main()
     int number3;
 
     cout<<"x+y = z\nPlease input x.\n";
-    cin>>number1;
+    cin>>number1; /*Get input */
+
     cin.clear();
     cout<<number1<<"+y = z\nPlease input y.\n";
     cin>>number2;
+
     cin.clear();
     number3 = number1 + number2;
     cout<<number1<<"+"<<number2<<" = "<<number3<<"\nMATH COMPLETED\n";
